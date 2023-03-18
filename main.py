d72b8373104b7fbca80a1fb06a1d6b9b337e21e8
AlinaKhusn <mingazova_al@mail.ru>
2023-03-18 22:20:36 +0300

    module12

---
+++ main.py
@@ -0,0 +1,10 @@
+money = int(input())
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = {per_cent['ТКБ']/100 * money,
+           per_cent['СКБ']/100 * money,
+           per_cent['ВТБ']/100 * money,
+           per_cent['СБЕР']/100 * money}
+deposit = list(map(round, deposit))
+print(deposit)
+deposit.sort(reverse = True)
+print(deposit[0])
